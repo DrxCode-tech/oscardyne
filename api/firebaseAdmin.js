2f20edbbc2de8dc83ac0377088924e1b1f90466a
DrxCode-tech <anietiedaniel11@gmail.com>
2025-11-24 21:20:01 +0100

    mdmf

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,20 +1,28 @@
 import admin from "firebase-admin";
 
-const serviceKey = process.env.FIREBASE_ADMIN_KEY;
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 console.log("KEY EXISTS?", process.env.FIREBASE_ADMIN_KEY ? "YES" : "NO");
 
 
-if (!serviceKey) {
+if (!serviceAccount) {
   throw new Error("FIREBASE_ADMIN_KEY is missing");
 }
 
-const parsedKey = JSON.parse(serviceKey);
+// FIX: convert \\n to real newlines
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(parsedKey)
+    credential: admin.credential.cert(serviceAccount)
   });
 }
 
 export const db = admin.firestore();
 export const auth = admin.auth();
+
+
+
+
+
+
+
